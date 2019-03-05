3d409f7b9e3a0b714c6a0d9a22aa45ce08a4c019
xahidbuffon <xahid.buffon@gmail.com>
2019-03-05 17:05:43 -0600

    trainX training

--- trainX.py
+++ trainX.py
@@ -4,33 +4,25 @@ from scipy import misc
 import numpy as np
 import argparse
 import random
-import glob
-import time
 import sys
 import cv2
 import os
+#export TF_CPP_MIN_LOG_LEVEL=2
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
 # my imports
-sys.path.insert(0, 'ops/')
-sys.path.insert(0, 'measures/')
 sys.path.insert(0, 'nets/')
-from tf_ops import *
-import data_ops
-import uiqm
-
+from utils.data_ops import getPaths, augment, preprocess 
+ 
 
 LEARNING_RATE = 1e-4
-UIQM_WEIGHT   = 0.0
 LOSS_METHOD   = 'wgan'
 BATCH_SIZE    = 16
 NUM_LAYERS    = 8
-L1_WEIGHT     = 100.0
-IG_WEIGHT     = 0
 NETWORK       = 'pix2pix'
 AUGMENT       = True
 EPOCHS        = 1
 DATA          = 'underwater_imagenet'
-LAB           = False
 
 EXPERIMENT_DIR  = 'checkpoints/'+LOSS_METHOD+'_'+NETWORK+'_'+DATA+'/'
 IMAGES_DIR      = EXPERIMENT_DIR+'samples/'
@@ -63,20 +55,15 @@ if NETWORK == 'resnet': from resnet import *
 
 # global step that is saved with a model to keep track of how many steps/epochs
 global_step = tf.Variable(0, name='global_step', trainable=False)
-
 # underwater image
 image_u = tf.placeholder(tf.float32, shape=(BATCH_SIZE, 256, 256, 3), name='image_u')
-
 # correct image
 image_r = tf.placeholder(tf.float32, shape=(BATCH_SIZE, 256, 256, 3), name='image_r')
-
 # generated corrected colors
 layers     = netG8_encoder(image_u)
 gen_image  = netG8_decoder(layers)
-
 # send 'clean' water images to D
 D_real = netD(image_r, LOSS_METHOD)
-
 # send corrected underwater images to D
 D_fake = netD(gen_image, LOSS_METHOD, reuse=True)
 
@@ -87,18 +74,18 @@ if LOSS_METHOD == 'least_squares':
     errD_fake = tf.nn.sigmoid(D_fake)
     errG = 0.5*(tf.reduce_mean(tf.square(errD_fake - 1)))
     errD = tf.reduce_mean(0.5*(tf.square(errD_real - 1)) + 0.5*(tf.square(errD_fake)))
-if LOSS_METHOD == 'gan':
+    n_critic = 1
+elif LOSS_METHOD == 'wgan':
+    # cost functions
+    errD = tf.reduce_mean(D_real) - tf.reduce_mean(D_fake)
+    errG = -tf.reduce_mean(D_fake)
+    n_critic = 5
+else:
     print 'Using original GAN loss'
     errD_real = tf.nn.sigmoid(D_real)
     errD_fake = tf.nn.sigmoid(D_fake)
     errG = tf.reduce_mean(-tf.log(errD_fake + e))
     errD = tf.reduce_mean(-(tf.log(errD_real+e)+tf.log(1-errD_fake+e)))
-if LOSS_METHOD == 'wgan':
-    # cost functions
-    errD = tf.reduce_mean(D_real) - tf.reduce_mean(D_fake)
-    errG = -tf.reduce_mean(D_fake)
-    n_critic = 5
-
 
 # gradient penalty
 epsilon = tf.random_uniform([], 0.0, 1.0)
@@ -124,12 +111,11 @@ G_train_op = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE).minimize(errG, 
 D_train_op = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE).minimize(errD, var_list=d_vars)
 
 saver = tf.train.Saver(max_to_keep=2)
-
 init = tf.group(tf.local_variables_initializer(), tf.global_variables_initializer())
 sess = tf.Session()
 sess.run(init)
-
 summary_writer = tf.summary.FileWriter(EXPERIMENT_DIR+'/logs/', graph=tf.get_default_graph())
+
 
 ckpt = tf.train.get_checkpoint_state(EXPERIMENT_DIR)
 if ckpt and ckpt.model_checkpoint_path:
@@ -140,22 +126,81 @@ if ckpt and ckpt.model_checkpoint_path:
     except:
         print "Could not restore model"
         pass
-
 step = int(sess.run(global_step))
-
 merged_summary_op = tf.summary.merge_all()
 
 data_dir = "/mnt/data2/color_correction_related/datasets/"
-trainA_paths = data_ops.getPaths(data_dir+DATA+'/trainA/') # underwater photos
-trainB_paths = data_ops.getPaths(data_dir+DATA+'/trainB/') # normal photos (ground truth)
-test_paths   = data_ops.getPaths(data_dir+DATA+'/test/')
-val_paths    = data_ops.getPaths(data_dir+DATA+'/val/')
+trainA_paths = getPaths(data_dir+DATA+"/trainA/") # underwater photos
+trainB_paths = getPaths(data_dir+DATA+"/trainB/") # normal photos (ground truth)
+test_paths   = getPaths(data_dir+DATA+"/test/")
+val_paths    = getPaths(data_dir+DATA+"/val/")
+num_train, num_test, num_val = len(trainA_paths), len(test_paths), len(val_paths)
+print ("{0} training pairs\n".format(len(trainB_paths)))
 
-print len(trainB_paths),'training pairs'
-num_train = len(trainA_paths)
-num_test  = len(test_paths)
-num_val   = len(val_paths)
 
-epoch_num = step/(num_train/BATCH_SIZE)
+TOTAL_STEP = (EPOCHS*num_train/BATCH_SIZE)
+while step < TOTAL_STEP:
+    # pick random images every time for D
+    for itr in range(n_critic):
+        idx = np.random.choice(np.arange(num_train), BATCH_SIZE, replace=False)
+        batchA_paths = trainA_paths[idx]
+        batchB_paths = trainB_paths[idx]
+        batchA_images = np.empty((BATCH_SIZE, 256, 256, 3), dtype=np.float32)
+        batchB_images = np.empty((BATCH_SIZE, 256, 256, 3), dtype=np.float32)
+        
+        for i,(a,b) in enumerate(zip(batchA_paths, batchB_paths)):
+            a_img = misc.imread(a)
+            b_img = misc.imread(b)
+
+            # Data augmentation here - each has 50% chance
+            if AUGMENT: 
+                a_img, b_img = augment(a_img, b_img)
+            batchA_images[i, ...] = preprocess(a_img)
+            batchB_images[i, ...] = preprocess(b_img)
+            
+        sess.run(D_train_op, feed_dict={image_u:batchA_images, image_r:batchB_images})
+
+    # also get new batch for G
+    idx = np.random.choice(np.arange(num_train), BATCH_SIZE, replace=False)
+    batchA_paths = trainA_paths[idx]
+    batchB_paths = trainB_paths[idx]
+    batchA_images = np.empty((BATCH_SIZE, 256, 256, 3), dtype=np.float32)
+    batchB_images = np.empty((BATCH_SIZE, 256, 256, 3), dtype=np.float32)
+
+    for i,(a,b) in enumerate(zip(batchA_paths, batchB_paths)):
+        a_img = misc.imread(a)
+        b_img = misc.imread(b)
+        # Data augmentation here - each has 50% chance
+        if AUGMENT: 
+            a_img, b_img = augment(a_img, b_img)
+        batchA_images[i, ...] = preprocess(a_img)
+        batchB_images[i, ...] = preprocess(b_img)
+        
+
+    sess.run(G_train_op, feed_dict={image_u:batchA_images, image_r:batchB_images})
+    D_loss, G_loss, summary = sess.run([errD, errG, merged_summary_op], feed_dict={image_u:batchA_images, image_r:batchB_images})
+    summary_writer.add_summary(summary, step)
+    step += 1
+    print ("Step {0}/{1}: lossD: {2}, lossG: {3}".format(step, TOTAL_STEP, D_loss, G_loss)) 
+
+    if (step%5000==0):
+        print ("Saving model")
+        saver.save(sess, EXPERIMENT_DIR+'checkpoint-'+str(step))
+        saver.export_meta_graph(EXPERIMENT_DIR+"checkpoint-"+str(step)+".meta")
+
+        idx = np.random.choice(np.arange(num_val), BATCH_SIZE, replace=False)
+        batch_paths  = val_paths[idx]
+        batch_images = np.empty((BATCH_SIZE, 256, 256, 3), dtype=np.float32)
+        print ("Testing on validation data")
+        for i,a in enumerate(batch_paths):
+            a_img = misc.imread(a).astype("float32")
+            batch_images[i, ...] = preprocess(misc.imresize(a_img, (256, 256, 3)))
+
+        gen_images = np.asarray(sess.run(gen_image, feed_dict={image_u:batch_images}))
+        for i, (gen, real) in enumerate(zip(gen_images, batch_images)):
+            misc.imsave(IMAGES_DIR+str(step)+"_real.png", real)
+            misc.imsave(IMAGES_DIR+str(step)+"_gen.png", gen)
+            if(i>=5): break
+
 
 
