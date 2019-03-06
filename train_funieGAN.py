5d71d5885fc93b23f10de50774b088ea3a669c9c
xahidbuffon <xahid.buffon@gmail.com>
2019-03-06 01:56:46 -0600

    model saved

--- train_funieGAN.py
+++ train_funieGAN.py
@@ -1,81 +1,86 @@
-from __future__ import print_function, division
-import scipy
 
-from keras.datasets import mnist
-from keras_contrib.layers.normalization.instancenormalization import InstanceNormalization
-from keras.layers import Input, Dense, Reshape, Flatten, Dropout, Concatenate
-from keras.layers import BatchNormalization, Activation, ZeroPadding2D
-from keras.layers.advanced_activations import LeakyReLU
-from keras.layers.convolutional import UpSampling2D, Conv2D
-from keras.models import Sequential, Model
-from keras.optimizers import Adam
-import datetime
-import matplotlib.pyplot as plt
-import sys
-
-import numpy as np
 import os
-#export TF_CPP_MIN_LOG_LEVEL=2
-os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+import numpy as np
 
-###-----hyper-params-----
-epochs = 1
-batch_size = 16
-sample_interval=50
-
-###--------configure data-loader------------
+## local libs
+from nets.funieGAN import FUNIE_GAN
 from utils.data_loader import DataLoader
+from utils.plot_utils import save_val_samples_funieGAN
+
+## configure data-loader
 data_dir = "/mnt/data2/color_correction_related/datasets/"
 dataset_name = "underwater_imagenet"
 data_loader = DataLoader(data_dir, dataset_name)
-if not os.path.exists("data/samples/"):
-    os.makedirs("data/samples/")
 
-###--------load model arch------------
-from nets.funieGAN import FUNIE_GAN
+## create dir for log and (sampled) validation data
+samples_dir = "data/samples/"
+checkpoint_dir = "checkpoints/funieGAN/"
+if not os.path.exists(samples_dir):
+    os.makedirs(samples_dir)
+if not os.path.exists(checkpoint_dir):
+    os.makedirs(checkpoint_dir)
+
+## hyper-params
+num_epoch = 1
+batch_size = 16
+val_interval = 5
+save_model_interval = 10 #data_loader.num_train//batch_size
+num_step = num_epoch*save_model_interval
+
+## load model arch
 funie_gan = FUNIE_GAN()
 
-# Adversarial loss ground truths
+## ground-truths for adversarial loss
 valid = np.ones((batch_size,) + funie_gan.disc_patch)
 fake = np.zeros((batch_size,) + funie_gan.disc_patch)
 
 step = 0
-TOTAL_STEP = epochs*(data_loader.num_train/batch_size)
-while step < TOTAL_STEP:
+while (step <= num_step):
     for _, (imgs_A, imgs_B) in enumerate(data_loader.load_batch(batch_size)):
-        #  Train Discriminator
+        ##  train the discriminator
         fake_A = funie_gan.generator.predict(imgs_B)
         d_loss_real = funie_gan.discriminator.train_on_batch([imgs_A, imgs_B], valid)
         d_loss_fake = funie_gan.discriminator.train_on_batch([fake_A, imgs_B], fake)
         d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
 
-        # Train the generators
+        ## train the generator
         g_loss = funie_gan.combined.train_on_batch([imgs_A, imgs_B], [valid, imgs_A])
         step += 1
-        print ("Step {0}/{1}: lossD: {2}, lossG: {3}".format(step, TOTAL_STEP, d_loss[0], g_loss[0])) 
+        print ("Step {0}/{1}: lossD: {2}, lossG: {3}".format(step, num_step, d_loss[0], g_loss[0])) 
 
-        # validate and save generated samples at regular intervals 
-        if step % sample_interval == 0:
+        ## validate and save generated samples at regular intervals 
+        if (step % val_interval==0):
             imgs_A, imgs_B = data_loader.load_val_data(batch_size=3)
             fake_A = funie_gan.generator.predict(imgs_B)
             gen_imgs = np.concatenate([imgs_B, fake_A, imgs_A])
             gen_imgs = 0.5 * gen_imgs + 0.5 # Rescale to 0-1
-            save_samples(gen_imgs, step)
+            save_val_samples_funieGAN(samples_dir, gen_imgs, step)
+
+        if (step % save_model_interval==0):
+            ## save model and weights
+            model_name = checkpoint_dir+("model_%d" %step)
+            with open(model_name+"_.json", "w") as json_file:
+                json_file.write(funie_gan.generator.to_json())
+            funie_gan.generator.save_weights(model_name+"_.h5")
+            print("\nSaved trained model in {0}\n".format(checkpoint_dir))
 
 
-def save_samples(gen_imgs, step, row=3, col=3):
-    r, c = 3, 3
-    titles = ['Condition', 'Generated', 'Original']
-    fig, axs = plt.subplots(r, c)
-    cnt = 0
-    for i in range(r):
-        for j in range(c):
-            axs[i,j].imshow(gen_imgs[cnt])
-            axs[i, j].set_title(titles[i])
-            axs[i,j].axis('off')
-            cnt += 1
-    fig.savefig("data/samples/%d.png" % (step))
-    plt.close()
+def generate_from_saved_model():
+    """ For loading a saved model and generating an image
+    """
+    assert (os.path.exists("models/cgan_mnist.json")), "model does not exist"
+    assert (os.path.exists("models/cgan_mnist.h5")), "model does not exist"
+    from keras.models import model_from_json
+    # load json and create model
+    json_file = open('models/cgan_mnist.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    model = model_from_json(loaded_model_json)
+    # load weights into new model
+    model.load_weights("models/cgan_mnist.h5")
+    print("\nLoaded data and model")
+
+
 
 
 
