709617004bff7a805766e6097fbeed473e6a1720
xahid <xahid.buffon@gmail.com>
2020-08-24 09:52:55 +0600

    evals

--- Evaluation/measure_uiqm.py
+++ Evaluation/measure_uiqm.py
@@ -28,15 +28,15 @@ Get datasets from
  - http://irvlab.cs.umn.edu/resources/euvp-dataset
  - http://irvlab.cs.umn.edu/resources/ufo-120-dataset
 """
-#inp_dir = "/home/xahid/datasets/released/EUVP/test_samples/Inp/"
-inp_dir = "/home/xahid/datasets/released/UFO-120/TEST/lrd/"
+inp_dir = "/home/xahid/datasets/released/EUVP/test_samples/Inp/"
+#inp_dir = "/home/xahid/datasets/released/UFO-120/TEST/lrd/"
 ## UIQMs of the distorted input images 
 inp_uqims = measure_UIQMs(inp_dir)
 print ("Input UIQMs >> Mean: {0} std: {1}".format(np.mean(inp_uqims), np.std(inp_uqims)))
 
 ## UIQMs of the enhanceded output images
-#gen_dir = "eval_data/euvp_test/funie-gan/" 
-gen_dir = "eval_data/ufo_test/deep-sesr/" 
+gen_dir = "eval_data/euvp_test/funie-gan/" 
+#gen_dir = "eval_data/ufo_test/deep-sesr/" 
 gen_uqims = measure_UIQMs(gen_dir)
 print ("Enhanced UIQMs >> Mean: {0} std: {1}".format(np.mean(gen_uqims), np.std(gen_uqims)))
 
